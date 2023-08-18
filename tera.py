0e96a1c824153e3d1de525db88e468fbee88ef2e
CaiquedoCoutoRicardo <tad-caiquericardo@ucpparana.edu.br>
2023-08-18 20:17:19 -0300

    Create tera.py

---
+++ tera.py
@@ -0,0 +1,9 @@
+opcao = int(input("escolha uma opção para uma das frutas: 1- Maça 2-Banana 3-Laranja"))
+if opcao == 1:
+    print("maçã escolhida")
+elif opcao == 2:
+    print("banana escolhida")
+elif opcao == 3:
+    print("laranja escolhida")
+else:
+    print("opção invalida")
